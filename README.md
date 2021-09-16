6b21cac515e571a4f963dbf96140cbcfceee5852
liongkj <liongkj@sjtu.edu.cn>
2021-09-16 16:01:18 +0800

    change image display

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 # pubchem2excel tool to extract pub chem molecules, images, to html 
 
+## Limitation
+
+1. not tested thoroughly
+2. excel has no image
+
 ## How to use?
+
+0. Clone repo
+
+    ```bash
+    git clone https://github.com/liongkj/pubchem2excel.git && cd pubchem2excel
+    code .
+    ```
 
 1. Edit input.txt
 One element per line
 
 2. Install requirements
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
 3. Run script
 
     ```bash
-    pip -r requirements.txt
     python main.py
     ```
 
-4. Output format: HTML, excel
+4. Output format: HTML (with image), excel
--- main.py
+++ main.py
@@ -2,78 +2,147 @@ import pandas as pd
 import requests
 import json
 
-from IPython.core.display import display,HTML
+from PIL import Image
+from IPython.core.display import display, HTML
 import io
+
+from io import BytesIO
 from tqdm import tqdm
+import os
+import urllib
+
+import base64
 
 
 def main():
+    dir_base = os.getcwd()
     file = open("input.txt", "r")
-    nonempty_lines = [line.strip("\n") for line in file ]
-    line_count = len(nonempty_lines)
+    lines = file.readlines()
+    lines.sort()
+    # nonempty_lines = [line.strip("\n") for line in file]
+    line_count = len(lines)
+    print(line_count)
     file.close()
-    with open('input.txt', 'r') as file1:
-        df_prop_col = pd.DataFrame(columns=['name', 'structure', "IUPAC Name","Molecular Formula","Molecular Weight", "InChI","Log P","Hydrogen Bond Acceptor","Hydrogen Bond Donor"])
+    notfound = ""
+    with open("input.txt", "r") as file1:
+        df_prop_col = pd.DataFrame(
+            columns=[
+                "name",
+                "structure",
+                "IUPAC Name",
+                "Molecular Formula",
+                "Molecular Weight",
+                "InChI",
+                "Log P",
+                "Hydrogen Bond Acceptor",
+                "Hydrogen Bond Donor",
+            ]
+        )
         for index, d in enumerate(tqdm(file1, total=line_count)):
-            response = requests.get(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{d.strip().lower()}/json")
+            slugify = urllib.parse.quote(d.strip().lower())
+            response = requests.get(
+                f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{slugify}/json"
+            )
             # print
-            if (response.status_code == 200):
+            if response.status_code == 200:
                 res = response.json()
                 # print(res)
-                comp =res['PC_Compounds'][0]
+                comp = res["PC_Compounds"][0]
                 # print(comp.keys())
-                cid = comp["id"]['id']['cid']
-                props = comp['props']
-                prop_keys = ["IUPAC Name","Molecular Formula","Molecular Weight", "InChI", "Log P","Hydrogen Bond Acceptor","Hydrogen Bond Donor"]
-                prop_dic= {}
+                cid = comp["id"]["id"]["cid"]
+                props = comp["props"]
+                prop_keys = [
+                    "IUPAC Name",
+                    "Molecular Formula",
+                    "Molecular Weight",
+                    "InChI",
+                    "Log P",
+                    "Hydrogen Bond Acceptor",
+                    "Hydrogen Bond Donor",
+                ]
+                prop_dic = {}
                 for p in prop_keys:
                     for pr in props:
-                        if(p=="IUPAC Name") and pr["urn"]["label"] ==p and pr["urn"]["name"] == "Systematic":
-                            prop_dic[pr["urn"]["label"]]=pr["value"]["sval"]
-                        elif(p=="Log P") and pr["urn"]["label"]==p and 'name' in pr["urn"] and pr["urn"]["name"] == "XLogP3":
-                            prop_dic[pr["urn"]["label"]]=pr["value"]["fval"]
-                        elif(p.startswith("Hydrogen")) and pr["urn"]["label"]=="Count" :
+                        if (
+                            (p == "IUPAC Name")
+                            and pr["urn"]["label"] == p
+                            and pr["urn"]["name"] == "Systematic"
+                        ):
+                            prop_dic[pr["urn"]["label"]] = pr["value"]["sval"]
+                        elif (
+                            (p == "Log P")
+                            and pr["urn"]["label"] == p
+                            and "name" in pr["urn"]
+                            and pr["urn"]["name"] == "XLogP3"
+                        ):
+                            prop_dic[pr["urn"]["label"]] = pr["value"]["fval"]
+                        elif (p.startswith("Hydrogen")) and pr["urn"][
+                            "label"
+                        ] == "Count":
                             prop_dic[pr["urn"]["name"]] = pr["value"]["ival"]
-                        elif(p==pr["urn"]["label"]):
+                        elif p == pr["urn"]["label"]:
                             prop_dic[pr["urn"]["label"]] = pr["value"].values()
                 # prop_tup = [()]
                 # prop_list.append(prop_tup)
-                
-                structure = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{d.strip()}/PNG'
+
+                structure = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{slugify}/PNG"
                 # df_prop_col = pd.DataFrame.from_records(prop_tup,columns=prop_keys)
-                
+                image_name = f"{slugify}.jpg"
+                local_path_thumb = os.path.join(dir_base, "output", "img", image_name)
+                if not os.path.isfile(local_path_thumb):
+                    urllib.request.urlretrieve(structure, local_path_thumb)
 
-                image_cols = ['structure']  #<- define which columns will be used to convert to html
+                df_prop_col.loc[index] = [
+                    d.strip(),
+                    get_thumbnail(local_path_thumb),
+                    prop_dic.get("IUPAC Name"),
+                    prop_dic.get("Molecular Formula"),
+                    prop_dic.get("Molecular Weight"),
+                    prop_dic.get("InChI"),
+                    prop_dic.get("Log P"),
+                    prop_dic.get("Hydrogen Bond Acceptor"),
+                    prop_dic.get("Hydrogen Bond Donor"),
+                ]
 
-                # Create the dictionariy to be passed as formatters
-                format_dict = {}
-                for image_col in image_cols:
-                    format_dict[image_col] = path_to_image_html
-
-                # res2 = requests.get(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{cid}/assaysummary/csv")
-                # if (res2.status_code == 200):
-                #     df = pd.read_csv(io.StringIO(res2.content.decode('utf-8')))
-                #     print(df["Bioassay Type"])
-                #     # print(df)
-                df_prop_col.loc[index] = [d.strip(), structure, prop_dic["IUPAC Name"], prop_dic["Molecular Formula"], prop_dic["Molecular Weight"], prop_dic["InChI"],prop_dic["Log P"],prop_dic["Hydrogen Bond Acceptor"],prop_dic["Hydrogen Bond Donor"] ]
-            
-            elif (response.status_code == 404):
-                print("Result not found!")
+            elif response.status_code == 404:
+                print(f"{d.strip()} not found!")
+                notfound += f"{d.strip()}\n"
                 # Code here will react to failed requests
         print("Saving to excel and HTML")
-        pd.set_option('display.max_colwidth', None)
-        df_prop_col.to_excel('./output/index.xlsx', index=False)
+        pd.set_option("display.max_colwidth", None)
+        df_prop_col.to_excel("./output/index.xlsx", index=False)
         df_prop_col.index = df_prop_col.index + 1
-        df_prop_col.style.set_properties(**{'text-align': 'left'})
-        html = df_prop_col.to_html(escape=False ,formatters=format_dict)
+        df_prop_col.style.set_properties(**{"text-align": "left"})
+        html = df_prop_col.to_html(
+            escape=False, formatters={"structure": image_formatter}
+        )
+        nt_found = open("./output/notfound.txt", "w")
+        nt_found.write(notfound)
+        nt_found.close()
         text_file = open("./output/index.html", "w")
         text_file.write(html)
         text_file.close()
 
 
+def get_thumbnail(path):
+    # path = (
+    #     "\\\\?\\" + path
+    # )  # This "\\\\?\\" is used to prevent problems with long Windows paths
+    i = Image.open(path)
+    return i
 
-def path_to_image_html(path):
-    return '<img src="'+ path + '" width="250" >'
+
+def image_base64(im):
+    if isinstance(im, str):
+        im = get_thumbnail(im)
+    with BytesIO() as buffer:
+        im.convert("RGB").save(buffer, "jpeg")
+        return base64.b64encode(buffer.getvalue()).decode()
+
+
+def image_formatter(im):
+    return f'<img src="data:image/jpeg;base64,{image_base64(im)}">'
+
 
 if __name__ == "__main__":
     main()
